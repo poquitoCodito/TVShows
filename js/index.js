f898d2d04d851c0d3735c9673bcbb722307d4c16
Igor <luzerneim00@gmail.com>
2019-05-02 14:39:22 +0200

    add single page

--- js/index.js
+++ js/index.js
@@ -5,35 +5,45 @@ var url = "http://api.tvmaze.com/shows";
 xhttp.open("GET", url, true);
 xhttp.send();
 
-xhttp.onreadystatechange = function() {
+xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
         var data = JSON.parse(xhttp.response);
-    console.log(data);
 
 
-    data.forEach(function(movie){
-    var movieCard = document.createElement("div");
-    var movieImage = document.createElement("img");
-    movieImage.src = movie.image.original;
-    var movieElement = document.createElement("h4");
-    movieElement.classList.add("card-title");
-    var movieTitle = document.createTextNode(movie.name);
-    movieElement.appendChild(movieTitle);
-    var movieButton = document.createElement("a");
-    var movieText = document.createTextNode("click for more");
-    movieButton.classList.add("btn");
-    movieButton.appendChild(movieText);
-    movieButton.classList.add("btn-primary");
-    var container = document.getElementsByClassName("row")[0];
-    container.appendChild(movieCard);
-    movieCard.appendChild(movieImage);
-    movieImage.classList.add("card-img-top");
-    movieCard.appendChild(movieElement);
-    movieCard.classList.add("card");
-    movieCard.appendChild(movieButton);
-    });
+        data.forEach(function (movie) {
+            var movieCard = document.createElement("div");
+            var imageElement = document.createElement("img");
+            var cardBody = document.createElement("div");
+            var titleElement = document.createElement("h4");
+            var buttonElement = document.createElement("a");
 
+            var container = document.getElementsByClassName("row")[0];
+            var titleText = document.createTextNode(movie.name);
+            var buttonText = document.createTextNode("click for more");
 
+            imageElement.src = movie.image.original;
+            titleElement.appendChild(titleText);
+            buttonElement.appendChild(buttonText);
+
+            container.appendChild(movieCard);
+            movieCard.appendChild(imageElement);
+            movieCard.appendChild(cardBody);
+            cardBody.appendChild(titleElement);
+            cardBody.appendChild(buttonElement);
+
+            buttonElement.classList = 'btn-click btn';
+            imageElement.classList.add("card-img-top");
+            movieCard.classList.add("card");
+            titleElement.classList.add("card-title");
+            cardBody.classList.add("card-body");
+
+            buttonElement.setAttribute("data-show-id", movie.id);
+
+            buttonElement.addEventListener('click', function () {
+                var movieId = buttonElement.getAttribute('data-show-id');
+                localStorage.setItem('movieId', movieId);
+                window.location = 'single.html';
+            });
+        });
     }
-  };
-
+}
--- js/single.js
+++ js/single.js
@@ -1,37 +1,49 @@
-let movieId = localStorage.getItem('movieId');
-var request = $.ajax({
-    url: "http://api.tvmaze.com/shows/" + movieId,
-    method: 'GET',
-    data: {
-        embed: ["cast", "seasons"]
+var movieId = localStorage.getItem('movieId');
+var xhttp = new XMLHttpRequest();
+var url = "http://api.tvmaze.com/shows/" + movieId;
+
+
+xhttp.open("GET", url, true);
+xhttp.send();
+
+xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+        var data = JSON.parse(xhttp.response);
+
+        var imageElement = document.createElement("img");
+        imageElement.src = data.image.original;
+        var imageContainer = document.querySelector('.image-div');
+        imageContainer.appendChild(imageElement);
+
+        
+        var container = document.getElementsByClassName("summary-div")[0];
+        var summaryElement = document.createElement("div");
+        summaryElement.innerHTML = data.summary;
+
+        container.appendChild(summaryElement);
+
+
+        var genresElement = document.createElement("ul");
+        var genres = document.getElementsByClassName("genres-div")[0];
+        genres.appendChild(genresElement);
+        
+        data.genres.forEach(function(genre){
+            var genreEl = document.createElement('li');
+            var genreText = document.createTextNode(genre);
+            genreEl.appendChild(genreText);
+            genresElement.appendChild(genreEl);
+        });
+
+        var titleEl = document.createElement("div");
+        var titleText = document.createTextNode(data.name);
+        titleEl.appendChild(titleText);
+        var title = document.getElementsByClassName("title-div")[0];
+        title.appendChild(titleEl);
+
+        var ratingEl = document.createElement("div");
+        var ratingText = document.createTextNode(data.rating.average);
+        ratingEl.appendChild(ratingText);
+        var rating = document.getElementsByClassName("rating-div")[0];
+        rating.appendChild(ratingEl);
     }
-});
-
-request.done(msg => {
-    console.log(msg);
-    let titleDiv = $('<h1>');
-    titleDiv.text(msg.name);
-    $('.singlePage').prepend(titleDiv);
-    let imageDiv = $("<img>");
-    imageDiv.attr('src', msg.image.original);
-    $('.image-div').append(imageDiv);
-
-    let seasonsNum;
-    let seasons = $("<ul>");
-    msg._embedded.seasons.forEach(function (element) {
-        let seasonsItem = $('<li>');
-        seasonsItem.html(`<strong>Premiere date: </strong>${element.premiereDate};  <strong>End date: </strong>${element.endDate};`);
-        seasons.append(seasonsItem);
-    });
-    $('.seasons-div').append(seasons);
-
-    let cast = $('<ul>');
-    msg._embedded.cast.forEach(function (element) {
-        let castItem = $('<li>');
-        castItem.html(`<strong>Actors name: </strong>${element.person.name};`);
-        cast.append(castItem);
-    });
-    $('.cast-div').append(cast);
-});
-
-
+}
